590dcc1b9bc3fca794991fdf6307f4e12e2913fa
matvei-droid <matvei241011@gmail.com>
2026-01-22 01:00:32 +0300

    Update sub.py

--- api/sub.py
+++ api/sub.py
@@ -1,12 +1,13 @@
 from http.server import BaseHTTPRequestHandler
 import requests
 
-# Сюда вставь свою RAW ссылку из Gist (начинается на https://gist.githubusercontent.com/...)
+# Твоя рабочая ссылка, которую ты прислал
 GIST_URL = "https://gist.githubusercontent.com/matvei-droid/8f4b0f87c5b18011a55490fcac60f57f/raw/26ab2ed6884ff4223cc0e1f1a3566525821269ac/serverv.txt"
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path == '/api/sub':
+        # Проверяем, что запрос идет именно на /api/sub
+        if '/api/sub' in self.path:
             try:
                 res = requests.get(GIST_URL)
                 self.send_response(200)
